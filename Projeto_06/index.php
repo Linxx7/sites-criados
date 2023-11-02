a72662d4c8f7c1fccf284ac7b4c03d5cab01f757
Ricardo Lins <99371137+Linxx7@users.noreply.github.com>
2023-11-01 23:55:07 -0300

    Add files via upload

---
+++ Projeto_06/index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
+	<link href="css/style.css" rel="stylesheet">
+	<title>Projeto 06</title>
+</head>
+<body>
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+					<div class="icons-social">
+						<a href="https://facebook.com"><i class="fa-brands fa-facebook" aria-hidden="true"></i></a>
+						<a href="https://instagram.com"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
+					</div><!--icons-social-->
+					<div class="clear"></div>
+				<div class="logo">
+					Danki.Code
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+
+				<nav class="mobile">
+					<i class="fa fa-bars"></i>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+				<div class="clear"></div>
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem vindo ao nosso site <span>website!</span> <h2>
+				<p>Nullam facilisis urna nisl, vitae porta ex facilisis a. Donec non ex eget turpis accumsan consectetur at quis arcu. 
+					Sed fermentum facilisis tincidunt. Pellentesque sit amet nisl quis lectus rhoncus vestibulum. 
+					Vivamus pellentesque efficitur auctor. Quisque mauris metus, sollicitudin quis urna in, efficitur pharetra quam. Duis sed tempor justo, et pulvinar augue.
+					 Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+					 Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+					 Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+					 Phasellus gravida erat a nulla faucibus fermentum.</p>
+				<p>Nullam facilisis urna nisl, vitae porta ex facilisis a. Donec non ex eget turpis accumsan consectetur at quis arcu. 
+					Sed fermentum facilisis tincidunt. Pellentesque sit amet nisl quis lectus rhoncus vestibulum. 
+					Vivamus pellentesque efficitur auctor. Quisque mauris metus, sollicitudin quis urna in, efficitur pharetra quam. Duis sed tempor justo, et pulvinar augue.
+					 Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+					 Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+					 Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+					 Phasellus gravida erat a nulla faucibus fermentum.</p>
+				<a href="">Saiba mais!</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->
+	</section><!--main-->
+
+	<section class="planos">
+		<div class="chamada-planos">
+		<div class="center">
+			<h2>Confira nossos planos</h2>
+		</div>
+		</div><!--chamada-planos -->	
+		<section class="box-planos">
+			<div class="center">
+			
+				<div class="box-planos-single">
+				<div class="box-planos-wraper">
+				<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa-solid fa-circle-check"></i> ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div>
+				</div><!--box-planos-wraper-->
+			</div><!--box-planos-single-->
+
+			<div class="box-planos-single">
+			<div class="box-planos-wraper">
+				<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa-solid fa-circle-check"></i> ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div>
+				</div><!--box-planos-wraper-->
+			</div><!--box-planos-single-->
+
+			<div class="box-planos-single">
+			<div class="box-planos-wraper">
+				<h2>Lorem ipsum</h2>
+					<div class="pontos-plano">
+						<p><i class="fa-solid fa-circle-check"></i> ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+						<p><i class="fa-solid fa-circle-check"></i>Lorem ipsum silor domor</p>
+					</div><!--pontos-plano-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div>
+				</div><!--box-planos-wraper-->
+			</div><!--box-planos-single-->
+
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section>
+	<section class="parallax">
+		<div class="overlay-parallax"></div>
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+				<p>Nullam facilisis urna nisl, vitae porta ex facilisis a. Donec non ex eget turpis accumsan consectetur at quis arcu. 
+						Sed fermentum facilisis tincidunt. Pellentesque sit amet nisl quis lectus rhoncus vestibulum. 
+						Vivamus pellentesque efficitur auctor. Quisque mauris metus, sollicitudin quis urna in, efficitur pharetra quam. Duis sed tempor justo, et pulvinar augue.
+						Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+						Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+						Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						Phasellus gravida erat a nulla faucibus fermentum.
+				</p>
+				<p>Nullam facilisis urna nisl, vitae porta ex facilisis a. Donec non ex eget turpis accumsan consectetur at quis arcu. 
+						Sed fermentum facilisis tincidunt. Pellentesque sit amet nisl quis lectus rhoncus vestibulum. 
+						Vivamus pellentesque efficitur auctor. Quisque mauris metus, sollicitudin quis urna in, efficitur pharetra quam. Duis sed tempor justo, et pulvinar augue.
+						Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+						Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+						Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						Phasellus gravida erat a nulla faucibus fermentum.
+				</p>
+		</div>
+	</section>
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="border-efeito"></div>Entre em contato</h2>
+				<form>
+					<input type="text" placeholder="insira seu nome...">
+					<input type="email" placeholder="insira seu nome...">
+					<textarea placeholder="sua mensagem..."></textarea>
+					<input type="submit" value="enviar">
+				</form>
+			</div>
+
+			<div class="w50 time">
+				<h2> <div class="border-efeito"></div> Conheça nosso time</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><div class="info-usuario">
+						<p style="font-weight: bold;" >Ricardo Lins</p>
+						<p style="font-weight: italic;">Ceo/Instrutor</p>
+					</div>
+					</div><!--box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>	Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+						Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+						Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						Phasellus gravida erat a nulla faucibus fermentum.</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><div class="info-usuario">
+						<p style="font-weight: bold;" >Ricardo Lins</p>
+						<p style="font-weight: italic;">Ceo/Instrutor</p>
+					</div>
+					</div><!--box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>	Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+						Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+						Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						Phasellus gravida erat a nulla faucibus fermentum.</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><div class="info-usuario">
+						<p style="font-weight: bold;" >Ricardo Lins</p>
+						<p style="font-weight: italic;">Ceo/Instrutor</p>
+					</div>
+					</div><!--box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>	Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+						Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+						Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						Phasellus gravida erat a nulla faucibus fermentum.</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+					<div class="box-avatar"></div><div class="info-usuario">
+						<p style="font-weight: bold;" >Ricardo Lins</p>
+						<p style="font-weight: italic;">Ceo/Instrutor</p>
+					</div>
+					</div><!--box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>	Nullam fermentum felis eget mi mattis, a consectetur enim malesuada. Nullam tortor est, laoreet scelerisque ipsum non, interdum viverra lorem. 
+						Curabitur eget est eget lectus sagittis pellentesque. Nam tincidunt justo eros, id luctus lorem suscipit vitae. Proin quis orci augue. 
+						Cras bibendum consectetur lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						Phasellus gravida erat a nulla faucibus fermentum.</p>
+					</div><!--descricao-usuario-box-->
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+	<footer>
+		<p>Todos os direitos reservados ao projeto 06</p>
+	</footer>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+	<script>
+		$(function() {
+			$('nav.mobile i').click(function(){
+				var el= $(this).parent().find('ul');
+				if(el.is(':visible')==false){
+				el.fadeIn();
+				}else{
+					el.fadeOut();
+				}
+			})
+		})
+	</script>
+</body>
+</html>
